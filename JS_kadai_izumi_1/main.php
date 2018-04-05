5ab394fcce607ce4e1fab633984257629df2821d
Raimu Izumi <>
2018-04-06 01:04:03 +0900

    no message

---
+++ JS_kadai_izumi_1/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>5.12 HAPPY SUMMER WEDDING</title>
+</head>
+<body>
+
+<?php
+// ここは PHP の中で記載する
+$name = $_POST[’name’];
+// 変数で受け取ることで「扱いやすく」する
+echo $name;
+ ?>
+
+</body>
+</html>
